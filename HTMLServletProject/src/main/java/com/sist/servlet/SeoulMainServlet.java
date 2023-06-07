c9636a1f6a625dd8c08e3ee64d12ec5032b20d81
SIST <SIST@DESKTOP-CPMDO9H>
2023-06-07 18:02:03 +0900

    2023_06_02 servlet

---
+++ HTMLServletProject/src/main/java/com/sist/servlet/SeoulMainServlet.java
@@ -0,0 +1,27 @@
+package com.sist.servlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ *	WEB
+ *   java => jsp/servlet (Spring)
+ *   c# => 
+ */
+@WebServlet("/SeoulMainServlet")
+public class SeoulMainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
