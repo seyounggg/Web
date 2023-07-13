35a917e905b17f2f91f9ce177d88e0683d809ef8
seyounggg <syn_95@naver.com>
2023-07-13 16:15:34 +0900

    2023_07_13 공지사항 페이지 추가

--- JSPFinalProject/src/main/java/com/sist/vo/NoticeVO.java
+++ JSPFinalProject/src/main/java/com/sist/vo/NoticeVO.java
@@ -1,5 +1,83 @@
 package com.sist.vo;
 
+import java.util.*;
+/*
+ 이름                                      널?      유형
+ ----------------------------------------- -------- ----------------------------
+ NO                                        NOT NULL NUMBER
+ ID                                                 VARCHAR2(20)
+ NAME                                      NOT NULL VARCHAR2(51)
+ TYPE                                               NUMBER
+ SUBJECT                                   NOT NULL VARCHAR2(1000)
+ CONTENT                                   NOT NULL CLOB
+ REGDATE                                            DATE
+ HIT                                                NUMBER
+ */
 public class NoticeVO {
-
+	private int no,type,hit;
+	private String id,name,subject,content;
+	private Date regdate;
+	private String dbday, notice_type;
+	
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public String getDbday() {
+		return dbday;
+	}
+	public void setDbday(String dbday) {
+		this.dbday = dbday;
+	}
+	public String getNotice_type() {
+		return notice_type;
+	}
+	public void setNotice_type(String notice_type) {
+		this.notice_type = notice_type;
+	}
+	
 }
