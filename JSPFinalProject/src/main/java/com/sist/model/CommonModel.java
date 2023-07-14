0dbb61d6f7b8a0568d9762498eeac3e179dd7a7a
seyounggg <syn_95@naver.com>
2023-07-14 13:23:12 +0900

    2023_07_14 뉴스top7(footer)

--- JSPFinalProject/src/main/java/com/sist/model/CommonModel.java
+++ JSPFinalProject/src/main/java/com/sist/model/CommonModel.java
@@ -4,11 +4,22 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 import com.sist.dao.*;
 import com.sist.vo.*;
+import com.sist.manager.*;
 public class CommonModel {
 	public static void commonRequestData(HttpServletRequest request) {
 		// footer에 들어간다
 		FoodDAO dao=FoodDAO.newInstance();
 		// => 공지사항 => 최신뉴스
+		List<NewsVO> nList=NewsManager.newsSearchData("맛집");
+		for(NewsVO vo:nList) {
+			String title=vo.getTitle();
+			if(title.length()>15) {
+				title=title.substring(0,14)+"...";
+				vo.setTitle(title);
+			}
+			vo.setTitle(title);
+		}
+		request.setAttribute("nList", nList);
 		// => 방문맛집
 		List<FoodVO> flist=dao.foodTop7();
 		request.setAttribute("flist", flist);
